6e8a135284df210ea20a3bbef741dd8a6e79f9eb
Daniel Moisoff <dmoisoff@gmail.com>
2018-10-10 23:20:21 -0400

    installed a bunch of dependences

---
+++ src/counter/counter_test.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+// import { shallow } from "enzyme";
+// import counter from "counter";
+// import { configureAdapter } from "./../enzymeConfigure";
+//
+// configureAdapter();
+
+// describe("simple counting component", () => {});
--- src/shallowSmokeTest.test.js
+++ src/shallowSmokeTest.test.js
@@ -1,10 +1,42 @@
-import React from "React";
-import { shallow } from "enzyme";
+import React from "react";
+import { shallow, mount, debug, render, containsMatchingElement } from "enzyme";
+import {
+  StaticRouter,
+  MemoryRouter,
+  Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router";
 import App from "./App";
 import { configureAdapter } from "./enzymeConfigure";
+import MockRouter from "react-mock-router";
+import createRouterContext from "react-router-test-context";
 
 configureAdapter();
+//
+describe("main app page", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
 
-it("renders without crashing", () => {
-  shallow(<App />);
+  it("renders about", () => {
+    const context = createRouterContext({ location: { pathname: "/about" } });
+    const push = jest.fn();
+    const wrapper = mount(
+      <MockRouter push={push} location={"/about"} path={"/about"}>
+        <Route
+          render={props => (
+            <App
+              location={"/about"}
+              path={"/about"}
+              history={{ location: "/about" }}
+            />
+          )}
+        />
+      </MockRouter>
+    );
+    console.log(wrapper.debug());
+    expect(wrapper.find(App).exists()).toBe(true);
+  });
 });
