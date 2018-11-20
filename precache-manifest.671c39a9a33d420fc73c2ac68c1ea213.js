536de1e6ecfdf46ddfb3ce16a2edabe91ac11605
Daniel Moisoff <dmoisoff@gmail.com>
2018-11-19 20:26:21 -0500

    Updates

---
+++ precache-manifest.671c39a9a33d420fc73c2ac68c1ea213.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f615822bfa18f0f971fd",
+    "url": "/RandoStream/static/js/runtime~main.f615822b.js"
+  },
+  {
+    "revision": "b2e63bb4cc06787d22ad",
+    "url": "/RandoStream/static/js/main.b2e63bb4.chunk.js"
+  },
+  {
+    "revision": "91cd1b1ca1b02d486227",
+    "url": "/RandoStream/static/js/1.91cd1b1c.chunk.js"
+  },
+  {
+    "revision": "b2e63bb4cc06787d22ad",
+    "url": "/RandoStream/static/css/main.a3e77da7.chunk.css"
+  },
+  {
+    "revision": "ff7944a23310145d3722ebeb5eb2b574",
+    "url": "/RandoStream/index.html"
+  }
+];
